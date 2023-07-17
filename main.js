f0e47f6d9306d990181f72876dad461660ca700b
sireenha24-meet <sireenhallaq1@gmail.com>
2023-07-17 17:49:14 +0300

    done1

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ let i=0;
 function changeBackgroundColor1(color1,color2){
 	i+=1;
 	if(i%2==0){
-		document.body.style.backgroundColor=color1;
+		changeBackgroundColor(color1);
 	}
 	else{
-		document.body.style.backgroundColor=color2;
+		changeBackgroundColor(color2);
 	}
 
 }
